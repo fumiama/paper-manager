613bb354413ac5aaf00e4dcaa1fd45ce856a6569
源文雨 <41315874+fumiama@users.noreply.github.com>
2023-03-12 23:31:05 +0800

    update settings

--- frontend/vben/src/settings/encryptionSetting.ts
+++ frontend/vben/src/settings/encryptionSetting.ts
@@ -5,8 +5,8 @@ export const DEFAULT_CACHE_TIME = 60 * 60 * 24 * 7
 
 // aes encryption key
 export const cacheCipher = {
-  key: '_11111000001111@',
-  iv: '@11111000001111_',
+  key: 'xyY$#$%^&&56^$&Y&YR^fiu|\\{pop{Poi<:6%@#$^CJ&^^gHU',
+  iv: 'knG^&e43w@65&90i90*YH-0+{K][;]\\}|kIOUGftyDTRFuy',
 }
 
 // Whether the system cache is encrypted using aes
